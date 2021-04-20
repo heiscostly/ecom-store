e28878ae837fd518dc6e6a0c2df78c166e5a11d1
heiscostly <ayangcostly@gmail.com>
2021-04-20 09:27:19 +0100

    Added Routing to the hompage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
 import Hompage from './page/homepage/homepage.component';
+
+
+
+
+
+const HatsPage = () => (
+  <div>
+      <h1>HATS PAGE</h1>
+  </div>
+);
+
+
+
 
 function App() {
   return (
     <div >
-      <Hompage />
+      <Switch>
+      <Route exact path='/' component={Hompage} />
+      <Route  path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
